50c93e3be16a0bfcfd8b7e60ae5c6cffe3a7013c
Jay Kim <kimjaeh10@gmail.com>
2023-12-11 00:30:19 -0500

    Update myfuns.py

--- myfuns.py
+++ myfuns.py
@@ -26,33 +26,20 @@ def get_recommended_movies(new_user_ratings):
     return movies.head(10)
 
 def get_popular_movies(genre: str):
-    ratings = pd.read_csv('Rmat.csv', index_col=0)
+    file_name = 'top_10_movies_' + genre + '.csv'
 
-    # Replace 'm' in column names
-    ratings.columns = ratings.columns.str.replace('m', '')
+    try:
+        # Read the CSV file for the specified genre
+        top_movies = pd.read_csv(file_name)
+        
+        top_movies.rename(columns={"MovieID": "movie_id"}, inplace=True)
 
-    # Replace 'u' in index names
-    ratings.index = ratings.index.str.replace('u', '')
+        top_movies['Genres'] = top_movies['Genres'].apply(lambda x: '|'.join(eval(x)))
 
-    def calculate_popularity(avg_rating, num_rating, total_ratings):
-        return avg_rating * 0.7 + (num_rating / total_ratings) * 0.3
+        transformed_df = top_movies[['movie_id', 'Title', 'Genres']].rename(columns={"Title": "title", "Genres": "genres"})
+        
+        return transformed_df
 
-    avg_ratings = ratings.mean().to_dict()
-    num_ratings = ratings.count().to_dict()
-
-    # Filter movies by genre
-    genre_movies = movies[movies['genres'].str.contains(genre)]
-
-    # Calculate popularity for each movie in the genre
-    movie_scores = {}
-    for _, row in genre_movies.iterrows():
-        movie_id = str(row['movie_id'])
-        if movie_id in avg_ratings and movie_id in num_ratings:
-            movie_scores[movie_id] = calculate_popularity(avg_ratings[movie_id], num_ratings[movie_id], ratings.shape[0])
-
-    # Sort movies by popularity
-    sorted_movies = sorted(movie_scores.items(), key=lambda x: x[1], reverse=True)
-
-    # Return the top 10 movies in the genre
-    top_movies = [movies[movies['movie_id'] == int(movie_id)] for movie_id, _ in sorted_movies[:10]]
-    return pd.concat(top_movies)
+    except FileNotFoundError:
+        print(f"No data available for genre: {genre}")
+        return pd.DataFrame()
